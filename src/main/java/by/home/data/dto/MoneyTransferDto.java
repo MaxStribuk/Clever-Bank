a5563ad3a7e31c686cbc50049055703258fb3c29
Maxim <m.strybuk@astondevs.ru>
2023-08-24 13:53:34 +0300

    added validation for input data

--- src/main/java/by/home/data/dto/MoneyTransferDto.java
+++ src/main/java/by/home/data/dto/MoneyTransferDto.java
@@ -6,6 +6,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
@@ -16,7 +20,16 @@ import java.math.BigDecimal;
 @Builder
 public class MoneyTransferDto implements Serializable {
 
+    @Pattern(regexp = "^BY13[A-Z]{4}[0-9]{20}$", message = "invalid account number")
+    @NotBlank(message = "invalid accountFrom number")
     private String accountFrom;
+
+    @Pattern(regexp = "^BY13[A-Z]{4}[0-9]{20}$", message = "invalid account number")
+    @NotBlank(message = "invalid accountTo number")
     private String accountTo;
+
+    @NotNull(message = "amount must be positive or negative")
+    @Digits(integer = 8, fraction = 2,
+            message = "the amount must have no more than 8 digits in the integer part and 2 in the fractional")
     private BigDecimal amount;
 }
