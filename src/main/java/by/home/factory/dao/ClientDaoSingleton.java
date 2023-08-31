9e8e73c200bbd36e041121aa8a4ce466fabf9e8f
Maxim <m.strybuk@astondevs.ru>
2023-08-31 16:32:13 +0300

    created ClientDao

---
+++ src/main/java/by/home/factory/dao/ClientDaoSingleton.java
@@ -0,0 +1,23 @@
+package by.home.factory.dao;
+
+import by.home.dao.api.IClientDao;
+import by.home.dao.impl.ClientDao;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClientDaoSingleton {
+
+    private static volatile IClientDao instance;
+
+    public static IClientDao getInstance() {
+        if (instance == null) {
+            synchronized (ClientDaoSingleton.class) {
+                if (instance == null) {
+                    instance = new ClientDao();
+                }
+            }
+        }
+        return instance;
+    }
+}
