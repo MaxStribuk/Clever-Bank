0ac48fffb4f7215d3a2c04b2a06dff5f04ddefc1
Maxim <m.strybuk@astondevs.ru>
2023-08-31 16:32:47 +0300

    updated AccountService

--- src/main/java/by/home/service/api/IAccountService.java
+++ src/main/java/by/home/service/api/IAccountService.java
@@ -4,6 +4,7 @@ import by.home.dao.entity.Account;
 import by.home.data.dto.ChangeBalanceDto;
 import by.home.data.dto.MoneyTransferDto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface IAccountService {
@@ -14,5 +15,9 @@ public interface IAccountService {
 
     List<Account> getAccountsForInterestAccrual(int count, boolean status);
 
+    void interestAccrual(Account account, BigDecimal accrual);
+
     void update(Account account);
+
+    Account findByAccountNumber(String accountNumber);
 }
